9aa84275e6db30e355fc78027e1041e30e307213
Judy <jyu@lcs.on.ca>
2021-12-11 10:59:09 -0500

    Create a static layout with four buttons for the content view.

--- ReviewPractice/Views/ContentView.swift
+++ ReviewPractice/Views/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Stored properties
+    
+    //MARK: Computed properties
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            VStack(alignment: .center, spacing: 20) {
+                //question
+                Text("Question?")
+                //choice A
+                Button(action: {
+                    
+                }, label: {
+                    Text("A. ")
+                })
+                    .buttonStyle(.bordered)
+                
+                //choice B
+                Button(action: {
+                    
+                }, label: {
+                    Text("B. ")
+                })
+                    .buttonStyle(.bordered)
+                
+                //choice C
+                Button(action: {
+                    
+                }, label: {
+                    Text("C. ")
+                })
+                    .buttonStyle(.bordered)
+                
+                //choice D
+                Button(action: {
+                    
+                }, label: {
+                    Text("D. ")
+                })
+                    .buttonStyle(.bordered)
+            }
+            .navigationTitle("Multiple Choices")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
