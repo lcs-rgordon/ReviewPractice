b0f9d82ec220f8f058ed8cf6bbf7c0b709f1e184
Judy <jyu@lcs.on.ca>
2021-12-11 09:48:25 -0500

    Initial Commit

---
+++ ReviewPractice/ReviewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewPracticeApp.swift
+//  ReviewPractice
+//
+//  Created by Judy Yu on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct ReviewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
