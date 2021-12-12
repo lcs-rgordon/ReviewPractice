1893ccde919c066de0f844839786e905b6971cd0
Judy <jyu@lcs.on.ca>
2021-12-11 21:02:39 -0500

    Adjust the structured function

--- ReviewPractice/Views/ContentView.swift
+++ ReviewPractice/Views/ContentView.swift
@@ -11,9 +11,9 @@ struct GrowingButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding()
-            .background(Color.white)
+            .background(isPressed ? Color.white : Color.blue.opacity(0.5))
             .foregroundColor(.black)
-            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .scaleEffect(configuration.isPressed ? 1.1 : 1)
             .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
     }
 }
