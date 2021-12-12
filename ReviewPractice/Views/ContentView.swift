e138bd31dd9a9bb32dfac922d47ef113cc9856d4
Judy <jyu@lcs.on.ca>
2021-12-11 20:59:38 -0500

    add a structure for button style

--- ReviewPractice/Views/ContentView.swift
+++ ReviewPractice/Views/ContentView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+struct GrowingButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color.white)
+            .foregroundColor(.black)
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
 struct ContentView: View {
     
     //MARK: Stored properties
